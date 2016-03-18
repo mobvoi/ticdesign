536c898ec29b620a04b6c27c464e16eddba90066
Yongce Tu <yongce.tu@gmail.com>
2016-03-18 11:54:31 +0800

    Add Android.mk
    Change-Id: I83b9efce99207e8c095a5b0e95419c6ca61e6c12

---
+++ Android.mk
@@ -0,0 +1,4 @@
+
+LOCAL_PATH := $(call my-dir)
+include $(call all-subdir-makefiles)
+
---
+++ ticDesign/Android.mk
@@ -0,0 +1,20 @@
+
+LOCAL_PATH := $(call my-dir)
+
+include $(CLEAR_VARS)
+
+LOCAL_MODULE := ticwear-design-support
+
+#LOCAL_FULL_MANIFEST_FILE := $(LOCAL_PATH)/src/main/AndroidManifest.xml
+LOCAL_MANIFEST_FILE := src/main/AndroidManifest.xml
+
+LOCAL_RESOURCE_DIR := $(LOCAL_PATH)/src/main/res \
+	$(LOCAL_PATH)/src/main/res-public
+
+LOCAL_SRC_FILES := $(call all-java-files-under, src/main/java) \
+	$(call all-Iaidl-files-under, src/main/java)
+
+LOCAL_STATIC_JAVA_LIBRARIES := android-support-v4-latest android-support-v7-recyclerview-latest
+
+include $(BUILD_STATIC_JAVA_LIBRARY)
+
